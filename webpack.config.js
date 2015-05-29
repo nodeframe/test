722d5d548cb6de24d12a1f175b02790221ffcd13
thakerng <southbridge_855@hotmail.com>
2015-05-29 20:53:41 +0700

    Test Web

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    entry: "./entry.js",
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.jade$/, loader: "jade" },
+            { test: /\.less$/, loader: "style!css!less" },
+            { test: /\.css$/, loader: "style!css" }
+        ]
+    }
+};
